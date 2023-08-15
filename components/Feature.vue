c8a2709b23db9b5ab95b4ca3feeb376f642a9058
ilestis <jay.payne.ch@gmail.com>
2023-08-14 19:08:18 -0600

    More marketing on landing

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center gap-1.5">
-    <i v-bind:class="iconCss()" aria-hidden="true"></i>
+    <i v-bind:class="iconCss()" style="--fa-primary-color: #112B6B; --fa-secondary-color: #4A58FF;" aria-hidden="true"></i>
     <h3 class="text-center">{{title }}</h3>
     <p class="text-sm text-center">{{ lead }}</p>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -11,27 +11,42 @@
     </div>
   </BaseHero>
 
+  <Section id="pitch">
+    <div class="mx-auto lg:max-w-4xl flex flex-col gap-4">
+      <h2 class="text-purple">
+        A novel way to keep track of your worldbuilding and RPG campaigns
+      </h2>
+      <p>
+        Access Kanka from all of your devices, with your data always up to date. Forget about losing track of your scattered notes, in Kanka everything is in one place, easy to find, and accessible on the go.
+      </p>
+    </div>
+
+    <div class="text-center">
+      <img src="https://th.kanka.io/Vg8GkfblEvh1Z4HhO2Cwr4hzuCs=/900x562/smart/src/app/front/devices-preview-hd.png" class="inline" loading="lazy" alt="Kanka tabletop rpg campaign management and worldbuilding dashboard">
+    </div>
+  </Section>
+
   <Section id="features">
-    <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-4 text-dark">
+    <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-4 text-dark mb-4">
       <Feature
-        icon="fa-solid fa-dollar"
+        icon="fa-duotone fa-sack"
         title="Free to use"
-        lead="Get core features of Kanka for free - no more paying for unlimited campaigns or being restricted on elements."
+        lead="Our free tier isn't just a trial, it includes all of our core features and unlimited elements."
         />
       <Feature
-        icon="fa-solid fa-users"
+        icon="fa-duotone fa-users"
         title="Collaborative"
-        lead="Collaborate with friends in campaigns. Assign roles and control access to features and information."
+        lead="Collaborate with your friends by inviting them to the campaign and easily keep track of what they can interact with."
         />
       <Feature
-        icon="fa-solid fa-box"
-        title="Modular"
-        lead="Create customized campaigns with various features like characters, locations, quests, and more."
+        icon="fa-duotone fa-palette"
+        title="Customizable"
+        lead="Customize every last detail of a campaign. This can be accomplished by changing the built-in modules, or writing custom CSS."
         />
       <Feature
-        icon="fa-solid fa-rocket"
+        icon="fa-duotone fa-rocket"
         title="Frequent updates"
-        lead="We prioritize features our community loves, frequently updating kanka and keeping your data safe."
+        lead="We prioritize features our community loves, frequently updating Kanka and keeping your data safe."
         />
     </div>
 
@@ -87,6 +102,16 @@
       </div>
     </div>
   </Section>
+
+  <Section id="register">
+    <h2 class="text-purple">Like what you see?</h2>
+    <p>
+      <NuxtLink :to="`${runtimeConfig.app}/register`" class="btn-round rounded-full">
+        Register a free account now
+      </NuxtLink>
+    </p>
+  </Section>
+
   <img v-if="hasGclid()" v-bind:src="prepareUrl()" width="1" height="1" />
 </template>
 <script setup lang="ts">
