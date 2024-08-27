1cd394b1b0c0ba317d5c6631e87a00053d881ddc
ilestis <jay.payne.ch@gmail.com>
2024-08-27 12:00:23 -0600

    Fix favicon

--- app.vue
+++ app.vue
@@ -6,12 +6,15 @@ useHead({
   meta: [
     { name: 'description', content: 'Are you a game master, worldbuilder, or a storyteller? We offer a tabletop campaign manager and worldbuilding tool that makes it easy to organise, plan, and enjoy your TTRPG campaigns. We are community driven, and best of all, our core features are free!' }
   ],
+  link: [
+    {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+  ]
 })
 useSeoMeta({
   ogTitle: 'Kanka - Online tabletop RPG campaign manager and worldbuilding tool',
   ogDescription: 'Are you a game master, worldbuilder, or a storyteller? We offer a tabletop campaign manager and worldbuilding tool that makes it easy to organise, plan, and enjoy your TTRPG campaigns. We are community driven, and best of all, our core features are free!',
   ogUrl: 'https://kanka.io',
-  ogImage: 'https://kanka-user-assets.s3.amazonaws.com/app/front/preview-background.png',
+  ogImage: 'https://d3a4xjr8r2ldhu.cloudfront.net/app/front/preview-background.png',
   ogImageType: 'image/png',
   ogImageWidth: 1920,
   ogImageHeight: 1024,
