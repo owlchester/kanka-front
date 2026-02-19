81a98874fc9739087fa557a796b9bf842d53a4a9
Jay Payne <jay.payne.ch@gmail.com>
2026-02-19 05:54:26 -0600

    Remove canonical link from nuxt.config.ts
    Removed canonical link from app head configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            link: [
-                { rel: 'canonical', href: 'https://kanka.io' }
-            ]
-        }
-    },
     devtools: { enabled: false },
     modules: [
         '@nuxtjs/tailwindcss',
