3c3ae47d9fe55918e32bcc25a0ea6885d4e042ec
ilestis <jay.payne.ch@gmail.com>
2026-03-03 21:48:52 -0600

    inlineSSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
+    experimental: {
+        inlineSSRStyles: true,
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxtjs/google-fonts',
